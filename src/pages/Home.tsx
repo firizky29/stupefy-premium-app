c65b35e64061758eb1155f49933481345979c2ed
firizky29 <13520095@std.stei.itb.ac.id>
2022-11-29 21:38:21 +0700

    feat: added pagination, songform

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,132 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import ReactPaginate from 'react-paginate';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlay, faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+import '../assets/css/home.css';
 
-const array = [{id: 1, name: 'song1'}, {id: 2, name: 'song2'}, {id: 3, name: 'song3'}];
+library.add(faPlay, faTrash, faEdit);
+
+// const array = [{id: 1, name: 'song1'}, {id: 2, name: 'song2'}, {id: 3, name: 'song3'}];
+const array: any[] =  [];
 
 const Home = (props:any) => {
+    // const [users, setUsers] = useState([]);
+    const [totalPages, setTotalPages] = useState(1);
+    const [limit, setLimit] = useState("10");
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const onChangeLimit = (e: any) => {
+        setLimit(e);
+    }
+
+    const onPageChange = (data: any) => {
+        const page = data.selected + 1;
+        setCurrentPage(page);
+    }
+    
     return (
         <div className='content'>
             <div className="container-title">
                 Your Song
             </div>
-            <table className="contents">
-                <tbody>
-                {
-                    Array.isArray(array) ? array.map((item:any) => {
-                        return (
-                            <tr key={item.id}>
-                                <td>{item.name}</td>
-                            </tr>
-                        )
-                    }) : null
-
-                }
-                </tbody>
-            </table>
+            {
+                Array.isArray(array) && array.length > 0 ? (
+                    <div className="container">
+                        <div className="clearfix d-flex justify-content-end gap-4">
+                            <div className="align-self-center m-0 p-0">
+                                <p className="font-weight-light m-0 mr-2 text-light">Rows per page: </p>
+                            </div>
+                            <div className="btn-group align-self-center align-top">
+                                <DropdownButton
+                                    title={limit}
+                                    id="dropdown-menu-align-right"
+                                    variant="dark"
+                                    onSelect={onChangeLimit}
+                                    >
+                                    <Dropdown.Item eventKey="10" active={limit === "10"}>10</Dropdown.Item>
+                                    <Dropdown.Item eventKey="15" active={limit === "15"}>15</Dropdown.Item>
+                                    <Dropdown.Item eventKey="20" active={limit === "20"}>20</Dropdown.Item>
+                                    <Dropdown.Item eventKey="25" active={limit === "25"}>25</Dropdown.Item>
+                                    <Dropdown.Item eventKey="30" active={limit === "30"}>30</Dropdown.Item>
+                                </DropdownButton>
+                            </div>
+                        </div>
+                        <table className="contents">
+                            <thead>
+                                <tr>
+                                    <th className="text-center song-id">#</th>
+                                    <th className="text-center song-name">TITLE</th>
+                                    <th className="text-center created-at">DATE ADDED</th>
+                                    <th className="text-center song-action">ACTIONS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                Array.isArray(array) ? array.map((item:any) => {
+                                    return (
+                                            <tr className="content-entry" key={item.id}>
+                                                <td className='text-center col-md-1'>{item.id}</td>
+                                                <td className='text-center col-md-4'>{item.name}</td>
+                                                <td className='text-center col-md-4'>{item.name}</td>
+                                                <td className='text-center col-md-3'>
+                                                    <div className="btn-group">
+                                                        <button className="btn btn-primary">
+                                                            <FontAwesomeIcon icon="play" />
+                                                            <span className="action-name"> Play </span>
+                                                        </button>
+                                                        <button className="btn btn-secondary">
+                                                            <FontAwesomeIcon icon="edit" />
+                                                            <span className="action-name"> Edit </span>
+                                                        </button>
+                                                        <button className="btn btn-danger">
+                                                            <FontAwesomeIcon icon="trash" />
+                                                            <span className="action-name"> Delete </span>
+                                                        </button>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                    )
+                                }) : null
+                            }
+                            </tbody>
+                        </table>
+                        <div className="pagination clearfix ms-auto d-flex justify-content-center mt-3">
+                            <div className="align-middle align-self-center p-0 m-0 d-flex">
+                                <ReactPaginate
+                                    previousLabel={"<"}
+                                    nextLabel={">"}
+                                    breakLabel={"..."}
+                                    pageCount={totalPages}
+                                    marginPagesDisplayed={1}
+                                    pageRangeDisplayed={2}
+                                    onPageChange={onPageChange}
+                                    containerClassName={"pagination justify-content-center m-0"}
+                                    pageClassName={"page-item"}
+                                    pageLinkClassName={"page-link"}
+                                    previousClassName={"page-item"}
+                                    previousLinkClassName={"page-link"}
+                                    nextClassName={"page-item"}
+                                    nextLinkClassName={"page-link"}
+                                    breakClassName={"page-item"}
+                                    breakLinkClassName={"page-link"}
+                                    activeClassName={"active"}
+                                    forcePage={currentPage - 1}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="container p-0">
+                        <table className="contents">
+                            <tr>You dont have song yet </tr>
+                        </table>
+                    </div>
+                )
+            }
         </div>
     );
 };
--- src/pages/SongForm.tsx
+++ src/pages/SongForm.tsx
@@ -1,34 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../assets/css/form.css';
+import AddSongIcon from '../assets/img/icons-add-song-grey.png';
 
-const array = [{id: 1, name: 'song4'}, {id: 2, name: 'song2'}, {id: 3, name: 'song3'}];
+const array = [{ id: 1, name: 'song4' }, { id: 2, name: 'song2' }, { id: 3, name: 'song3' }];
 
 const SongForm = (props: any) => {
+    const submit = () => {
+        console.log('submit');
+    }
+
     return (
         <div className='content'>
-        <div className="container-title">
-            Your Song
-        </div>
-        <table className="contents">
-            <tbody>
-            {
-                Array.isArray(array) ? array.map((item:any) => {
-                    return (
-                        <tr key={item.id}>
-                            <td>{item.name}</td>
-                        </tr>
-                    )
-                }) : null
+            <div className="container-title">
+                Add Song
+            </div>
+            <form onSubmit={submit} encType="multipart/form-data" className='contents'>
+                <div className="input-group-0">
+                    <div className="input-container">
+                        <label>Title</label>
+                        <input type="text" name="song-title" id="song-title" placeholder="Song Title" required />
+                    </div>
+                </div>
 
-            }
-            </tbody>
-        </table>
-    </div>
+                <div className="input-container song-uploader">
+                    <label>Upload Song File</label>
+                    <div className="field-with-icon">
+                        <img src={AddSongIcon} alt="" />
+                        <div className="file-uploader">
+                            <input type="text" name="song-path" id="song-path" placeholder="Select song file" disabled required />
+                            <div className="upload-button">Select File</div>
+                        </div>
+                        <input type="file" name="song-file" id="song-file"
+                            accept="audio/*" required hidden />
+                    </div>
+
+                    <audio id="add-audio" controls >
+                        <source type="audio/mp3" id="song-preview" src="" />
+                        <source type="audio/ogg" id="song-preview" src="" />
+                        Your browser does not support the audio element.
+                    </audio>
+                </div>
+                <input type="submit" value="Submit" name="submit-button" id="submit" />
+            </form>
+        </div>
     );
 };
 
 SongForm.propTypes = {
-    
+
 };
 
 export default SongForm;
